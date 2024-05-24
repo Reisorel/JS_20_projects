ba68fe3ead29da93fc7dd034de949e6656798ae6
Francois Lerosier <francois.lerosier@gmail.com>
2024-05-24 15:24:16 +0200

    mid-quizz

--- 2. Quizz/app.js
+++ 2. Quizz/app.js
@@ -1,4 +1,14 @@
 const responses = ["c", "a", "b", "a", "c"];
 const emojis = ["✔️", "✨", "👀", "😭", "👎"];
 
-console.log("Hello");
+const form = document.querySelector(".quiz-form");
+form.addEventListener("submit", handleSubmit)
+
+function handleSubmit(e) {
+  e.preventDefault()
+
+  const results = [];
+
+  const radioButtons = document.querySelectorAll("input[type='radio']:checked")
+  console.log(radioButtons);
+}
